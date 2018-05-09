708d5a19d82ddff4f4ab1f87f0dae898c5755f35
TYSS <singhdeepa9555@gmail.com>
2018-05-09 12:15:53 +0530

    2nd commit

---
+++ src/main/java/com/framework/generics/Autoconstant.java
@@ -0,0 +1,10 @@
+package com.framework.generics;
+
+public interface Autoconstant 
+{
+String GECKO_KEY="webdriver.gecko.driver";
+String GECKO_VALUE="./drivers/geckodriver.exe";
+String Path="./data/input.xlsx";
+
+
+}
